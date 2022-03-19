6c13f5bd627653b2cff6ceca0e3eed1e0f69fc69
roma_marshall <roma_marshall@outlook.com>
2022-03-19 13:49:42 +0100

    Init commit

---
+++ src/components/PostForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent>
+    <h4>New post creating</h4>
+    <my-input
+        v-model="post.title"
+        type="text"
+        placeholder="Title"
+    />
+    <my-input
+        v-model="post.body"
+        type="text"
+        placeholder="Description"
+    />
+    <my-button
+        style="align-self: flex-end; margin-top: 15px;"
+        @click="createPost"
+
+    >
+      Create
+    </my-button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+
+</style>
